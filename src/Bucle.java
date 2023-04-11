5ad8417d4ba53cbd038fd7cc5ed6247afb5db1f6
soyleny <piyiyi03@gmail.com>
2023-04-10 20:58:45 -0500

    trabajo_7_bootcamp

---
+++ src/Bucle.java
@@ -0,0 +1,13 @@
+public class Bucle {
+    public static void main(String[] args) {
+
+        String[] familia = {"David", "Harry", "Nicole", "Viviana"};
+
+        for (int i = 0; i < familia.length; i++) {
+
+            System.out.printf(familia[i] + ", ");
+        }
+
+    }
+
+}
